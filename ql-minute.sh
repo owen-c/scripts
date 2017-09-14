c07d2ce0d8fc67d9b209eeb3e35cebeb47df3678
owen-c <owen.cummings@gmail.com>
2017-09-14 15:26:30 -0700

    Minor tweak

--- ql-minute.sh
+++ ql-minute.sh
@@ -2,18 +2,18 @@
 
 HOUR=$1
 SEARCH_STRING="^Operation"
+#SEARCH_STRING="^RemoteIp"
 
 echo "" > output.txt
 
 for i in `seq 0 9`;
 do
     echo "$HOUR:0$i" >> output.txt
-    qlgrep "^EndTime.*$HOUR:0$i:" service_log*-0$1* | grep "$SEARCH_STRING" | sort | uniq -c >> output.txt
+    qlgrep "^EndTime.*$HOUR:0$i:" service_log*-$1* | grep "$SEARCH_STRING" | sort | uniq -c >> output.txt
 done
 
 for i in `seq 10 59`;
 do
     echo "$HOUR:$i" >> output.txt
-    qlgrep "^EndTime.*$HOUR:$i:" service_log*-0$1* | grep "$SEARCH_STRING" | sort | uniq -c >> output.txt
+    qlgrep "^EndTime.*$HOUR:$i:" service_log*-$1* | grep "$SEARCH_STRING" | sort | uniq -c >> output.txt
 done
-
